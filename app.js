f80b1ef88c534d5f3fbd57a39648a8415fdbccc0
LuckyFox <noah.grebet@orange.fr>
2023-01-23 20:50:51 +0100

    :art: Set server port to SERVER_PORT env variable

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const fastify = require('fastify');
 
 const app = fastify();
 
 function start(){
 	try {
-		app.listen({port: 3000})
+		app.listen({port: process.env.SERVER_PORT})
 			.then((address) => {
 				console.log(`Server listening on ${address}`);
 			});
