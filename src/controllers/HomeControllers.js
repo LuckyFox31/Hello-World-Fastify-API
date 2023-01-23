b73f472f3ff026f123e0f38e41b9a9174b05c255
LuckyFox <noah.grebet@orange.fr>
2023-01-23 21:16:51 +0100

    :sparkles: Add sayHelloToSomeoneHomeController

--- src/controllers/HomeControllers.js
+++ src/controllers/HomeControllers.js
@@ -16,6 +16,22 @@ async function welcomeHomeController(request, reply){
 	}
 }
 
+async function sayHelloToSomeoneHomeController(request, reply){
+	try{
+		const {name} = request.body;
+
+		return reply
+			.code(200)
+			.send({
+				status: 200,
+				message: `Hello ${name} !`
+			});
+	} catch (error){
+		errorHandler(reply, 500, error);
+	}
+}
+
 module.exports = {
-	welcomeHomeController
+	welcomeHomeController,
+	sayHelloToSomeoneHomeController
 }
