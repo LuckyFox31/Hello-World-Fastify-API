b12db85474457e2173a869cf31629105d46eeb9c
LuckyFox <noah.grebet@orange.fr>
2023-01-24 14:35:09 +0100

    :art: Add host in listen app

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 
 const fastify = require('fastify');
 
-const app = fastify();
+const app = fastify({logger: true});
 
 app.register(require('./src/routes/HomeRoutes'), {prefix: '/'});
 
 function start(){
 	try {
-		app.listen({port: process.env.SERVER_PORT})
+		app.listen({port: process.env.SERVER_PORT, host: process.env.SERVER_HOST})
 			.then((address) => {
 				console.log(`Server listening on ${address}`);
 			});
